144d2e9a5cbe2c72e989d5ff6e7cda464d68c566
phillipeastwood <1093482742@qq.com>
2020-03-06 09:45:14 +0800

    commit files

---
+++ src/main/java/com/example/victory/VictoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.victory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictoryApplication.class, args);
+	}
+
+}
