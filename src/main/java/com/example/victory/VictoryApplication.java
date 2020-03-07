96018a61f96bf726ba706d6d2e7e59292b0952ae
phillipeastwood <1093482742@qq.com>
2020-03-07 20:24:34 +0800

    commit

--- src/main/java/com/example/victory/VictoryApplication.java
+++ src/main/java/com/example/victory/VictoryApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class VictoryApplication {
 
@@ -13,10 +12,5 @@ public class VictoryApplication {
 		SpringApplication.run(VictoryApplication.class, args);
         System.out.printf("111");
 	}
-
-    @RequestMapping(value = "/getMessage")
-    public String getMessage(){
-        return "Hello world";
-    }
 
 }
--- src/main/java/com/example/victory/web/MessageController.java
+++ src/main/java/com/example/victory/web/MessageController.java
@@ -3,11 +3,11 @@ package com.example.victory.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 public class MessageController {
 
-//    @RequestMapping(value = "/getMessage")
-    public String getMessage(){
+    @RequestMapping(value = "/getMessage")
+    public String getMessage() {
         return "Hello world";
     }
 }
