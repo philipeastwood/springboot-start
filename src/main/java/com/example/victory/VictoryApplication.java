25e5d65aeed03a2b994006b142d7f5c648a93534
phillipeastwood <1093482742@qq.com>
2020-03-08 11:38:36 +0800

    update VictoryApplication

--- src/main/java/com/example/victory/VictoryApplication.java
+++ src/main/java/com/example/victory/VictoryApplication.java
@@ -2,15 +2,13 @@ package com.example.victory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VictoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VictoryApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(VictoryApplication.class, args);
         System.out.printf("111");
-	}
+    }
 
 }
