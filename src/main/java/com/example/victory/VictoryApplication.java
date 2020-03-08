5f84671ba6228f278b3f562f299832b77f600258
phillipeastwood <1093482742@qq.com>
2020-03-08 15:46:50 +0800

    commit VictoryApplication

--- src/main/java/com/example/victory/VictoryApplication.java
+++ src/main/java/com/example/victory/VictoryApplication.java
@@ -1,13 +1,18 @@
 package com.example.victory;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VictoryApplication {
 
+    @Value("serverPort")
+    private static String port ;
+
     public static void main(String[] args) {
         SpringApplication.run(VictoryApplication.class, args);
+        System.out.println("Port:" + port);
     }
 
 }
