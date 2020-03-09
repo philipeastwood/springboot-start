1dbca7aa1ad6b9d1420f580409f7a1f92d6cb9fc
phillipeastwood <1093482742@qq.com>
2020-03-09 10:20:42 +0800

    update some codes

--- src/main/java/com/example/victory/web/MessageController.java
+++ src/main/java/com/example/victory/web/MessageController.java
@@ -1,13 +1,20 @@
 package com.example.victory.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
 
+//    @Value("serverPort")
+//    public String port ;
+
+    @Value("${some.message}")
+    public String message ;
+
     @RequestMapping(value = "/getMessage")
     public String getMessage() {
-        return "Hello world";
+        return "some message:" + message;
     }
 }
